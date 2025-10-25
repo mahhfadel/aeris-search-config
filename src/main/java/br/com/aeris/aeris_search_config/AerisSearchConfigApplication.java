9acbfe09b447c84f4ca0824f4d56b54e9db2ba64
Maria Clara Fadel <mariaclarapereira@alunos.utfpr.edu.br>
2025-10-24 23:08:13 -0300

    initializr spring boot microservice

---
+++ src/main/java/br/com/aeris/aeris_search_config/AerisSearchConfigApplication.java
@@ -0,0 +1,13 @@
+package br.com.aeris.aeris_search_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerisSearchConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AerisSearchConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aeris/aeris_search_config/AerisSearchConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aeris.aeris_search_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerisSearchConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
